290315931160da6df798e0a71172440f6138c33a
Viverino1 <Vivek.maddineni@gmail.com>
2023-06-27 14:34:18 -0500

    working on GSAP

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
+import gsap from "gsap";
+import { useRef, useEffect } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+const logo = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/446b1af0-e6ba-4b0f-a9de-6ae6d3ed27a3/dfjhqn3-23d30b9d-16e3-42b6-aa12-e010a3999ef6.png/v1/fill/w_736,h_736,q_80,strp/satoru_gojo_aesthetic_pfp_by_harvester0fs0uls_dfjhqn3-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzM2IiwicGF0aCI6IlwvZlwvNDQ2YjFhZjAtZTZiYS00YjBmLWE5ZGUtNmFlNmQzZWQyN2EzXC9kZmpocW4zLTIzZDMwYjlkLTE2ZTMtNDJiNi1hYTEyLWUwMTBhMzk5OWVmNi5wbmciLCJ3aWR0aCI6Ijw9NzM2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.F7_Ce5Ih1dhahsCnvFHRZgivj_8AByd9ZYHS3Ju0aws "
+
 function App() {
-
-  const arr = [];
-  for (let i = 0; i < 2; i++) {
-    arr.push(i);
-  }
-
-  return(
-    <div className="fixed w-full h-screen overflow-auto">
-      {arr.map((e) => (
-        <div key={e} className="w-full h-full border-2 transition-all duration-300">
-          <img className="w-full h-full object-fit" src="https://www.autocar.co.uk/sites/autocar.co.uk/files/styles/gallery_slide/public/images/car-reviews/first-drives/legacy/10-porsche-718-cayman-gt4-rs-top-10.jpg?itok=BacF-43A"/>
-        </div>
-      ))}
+  const imgRef = useRef(null);
+  useEffect(() => {
+    const el = imgRef.current;
+    gsap.fromTo(
+      el,
+      {
+        css: {rotate: 0},
+      },
+      {
+        css: {rotate: 180},
+        scrollTrigger: {
+          trigger: el,
+          start: "top",
+          scrub: true,
+          markers: true,
+        },
+      }
+    );
+  }, []);
+  return (
+    <div className="w-full h-full overflow-auto">
+      <div className="w-fit h-fit rounded-full overflow-clip" ref={imgRef}>
+        <img src={logo} className="" alt="logo"/>
+      </div>
+      <div className="h-[100vh] bg-red-500"/>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
