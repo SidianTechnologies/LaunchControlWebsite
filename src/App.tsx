03b2149c1f6c902a2f07d3e558432521b7ec758d
Viverino1 <Vivek.maddineni@gmail.com>
2023-06-28 09:32:56 -0500

    only first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ArrowDown } from "react-bootstrap-icons";
+
 gsap.registerPlugin(ScrollTrigger);
 
 const img = "https://images.squarespace-cdn.com/content/v1/5f7ca9b4bb17060b028086bb/1680124033120-76B8OVF7MUX5DCP221C8/639408.jpg?format=1000w"
 
 function App() {
   return (
-    <div className="fixed w-full h-screen text-orange-200 overflow-auto bg-stone-950">
-      <div className="w-full h-full">
+    <div className=" w-full h-screen text-orange-200 overflow-auto">
+      <div className="relative w-full h-full">
         <div className="absolute w-full h-full">
           <div className="absolute z-10 w-1/2 h-full bg-gradient-to-l from-0% from-stone-950"/>
           <div className="absolute z-10 w-1/2 h-1/3 bottom-0 bg-gradient-to-t from-0% from-stone-950"/>
