3cd2668d6ab0bdab2848dd58a770de4b32fcf10c
Viverino1 <Vivek.maddineni@gmail.com>
2023-06-27 20:09:26 -0500

    face

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,30 @@
 import gsap from "gsap";
-import { useRef, useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ArrowDown } from "react-bootstrap-icons";
 gsap.registerPlugin(ScrollTrigger);
 
-const logo = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/446b1af0-e6ba-4b0f-a9de-6ae6d3ed27a3/dfjhqn3-23d30b9d-16e3-42b6-aa12-e010a3999ef6.png/v1/fill/w_736,h_736,q_80,strp/satoru_gojo_aesthetic_pfp_by_harvester0fs0uls_dfjhqn3-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NzM2IiwicGF0aCI6IlwvZlwvNDQ2YjFhZjAtZTZiYS00YjBmLWE5ZGUtNmFlNmQzZWQyN2EzXC9kZmpocW4zLTIzZDMwYjlkLTE2ZTMtNDJiNi1hYTEyLWUwMTBhMzk5OWVmNi5wbmciLCJ3aWR0aCI6Ijw9NzM2In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.F7_Ce5Ih1dhahsCnvFHRZgivj_8AByd9ZYHS3Ju0aws "
+const img = "https://images.squarespace-cdn.com/content/v1/5f7ca9b4bb17060b028086bb/1680124033120-76B8OVF7MUX5DCP221C8/639408.jpg?format=1000w"
 
 function App() {
-  const imgRef = useRef(null);
-  useEffect(() => {
-    const el = imgRef.current;
-    gsap.fromTo(
-      el,
-      {
-        css: {rotate: 0},
-      },
-      {
-        css: {rotate: 180},
-        scrollTrigger: {
-          trigger: el,
-          start: "top",
-          scrub: true,
-          markers: true,
-        },
-      }
-    );
-  }, []);
   return (
-    <div className="w-full h-full overflow-auto">
-      <div className="w-fit h-fit rounded-full overflow-clip" ref={imgRef}>
-        <img src={logo} className="" alt="logo"/>
+    <div className="fixed w-full h-screen text-orange-200 overflow-auto bg-stone-950">
+      <div className="w-full h-full">
+        <div className="absolute w-full h-full">
+          <div className="absolute z-10 w-1/2 h-full bg-gradient-to-l from-0% from-stone-950"/>
+          <div className="absolute z-10 w-1/2 h-1/3 bottom-0 bg-gradient-to-t from-0% from-stone-950"/>
+          <img className="w-full h-full object-cover -translate-x-[50vw] scale-x-[-1]" src={img}/>
+        </div>
+
+        <div className="absolute z-20 flex flex-col items-end w-full h-full">
+          <div className="w-2/3 h-full flex flex-col justify-center items-center space-y-4 text-center">
+            <div className="w-fit h-fit flex flex-col space-y-2 justify-center items-center">
+              <div className="text-8xl font-extrabold">Launch Control</div>
+              <div className="text-3xl">Lafayette High School Car Club</div>
+              <ArrowDown size={30}/>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="h-[100vh] bg-red-500"/>
     </div>
   );
 }
