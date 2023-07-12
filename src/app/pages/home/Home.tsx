0fab5e625e21af30ea15a88aa90ba61ad4c61b5d
Viverino1 <Vivek.maddineni@gmail.com>
2023-07-12 10:21:42 -0500

    fixed app structure

---
+++ src/app/pages/home/Home.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Landing from "./components/Landing";
+import AboutUs from "./components/AboutUs";
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Home(){
+  const fullPage = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(fullPage.current,
+      {
+        opacity: 1,
+      },
+      {
+        opacity: 1,
+        duration: 3,
+      }
+    );
+  }, []);
+
+  return(
+    <div className="w-full h-full flex flex-col justify-center items-center overflow-y-auto overflow-x-clip text-text bg-background" ref={fullPage}>
+      <Landing/>
+      <AboutUs/>
+    </div>
+  )
+}
---
+++ src/app/pages/home/components/AboutUs.tsx
@@ -0,0 +1,31 @@
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+
+export default function AboutUs(){
+  const aboutUsFirstText = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(aboutUsFirstText.current, 
+      {
+        opacity: 0,
+      }, {
+        opacity: 1,
+        scrollTrigger: {
+          scrub: true,
+          trigger: aboutUsFirstText.current,
+          end: "top 20%"
+        }
+      })
+  }, [])
+
+  return(
+    <div className="h-screen w-full text-center flex flex-col items-center" ref={aboutUsFirstText}>
+      <div className="text-6xl text-text-light font-bold">Who Are We?</div>
+      <div className="text-xl w-2/3 mt-space">{String("Launch control is a decent car club in Lafayette high school. ").repeat(10)}</div>
+      <div className="flex space-x-space">
+        <button className="button-secondary mt-space">Learn More</button>
+        <button className="button-primary mt-space">Learn More</button>
+      </div>
+    </div>
+  )
+}
