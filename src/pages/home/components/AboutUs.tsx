130d9b633d65ffe596b028308a701e2423e4dc6a
Viverino1 <Vivek.maddineni@gmail.com>
2023-07-16 14:17:18 -0500

    final commit of first crunch meeting

--- src/pages/home/components/AboutUs.tsx
+++ src/pages/home/components/AboutUs.tsx
@@ -10,14 +10,15 @@ export default function AboutUs(){
       whileInView={{opacity: 1, y: 0}}
       transition={{duration: animationDuration, ease: "backInOut"}}
       viewport={{once: true}}
+      
       className="text-6xl text-text-light font-bold">Who Are We?</motion.div>
 
       <motion.div 
       initial={{opacity: 0, y: offsetDistance}}
       whileInView={{opacity: 1, y: 0}}
-      transition={{duration: animationDuration, ease: "backInOut", delay: animationDuration/4}}
+      transition={{duration: animationDuration, ease: "backInOut"}}
       viewport={{once: true}}
-      className="text-xl w-full md:w-2/3 px-4 mt-space">{String("Launch control is a decent car club in Lafayette high school. ").repeat(10)}</motion.div>
+      className="text-xl w-full md:w-2/3 px-4 mt-space">{String("Launch control is a decent car club in Lafayette high school. ").repeat(5)}</motion.div>
     </motion.div>
   )
 }
--- src/pages/home/components/Timeline.tsx
+++ src/pages/home/components/Timeline.tsx
@@ -22,6 +22,13 @@ const nodes: TimelineNode[] = [
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque felis lacus, interdum et bibendum ac, elementum eget turpis. Maecenas ultrices felis at mauris facilisis viverra. Nullam sit amet nisl nec mauris ultricies facilisis vitae quis nulla. Nunc eu ligula imperdiet, tincidunt mauris ac, tincidunt eros.",
     img: "./EngineHorizontal2.jpg",
   },
+  {
+    time: "Early July",
+    title: "Buying parts",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque felis lacus, interdum et bibendum ac, elementum eget turpis. Maecenas ultrices felis at mauris facilisis viverra. Nullam sit amet nisl nec mauris ultricies facilisis vitae quis nulla. Nunc eu ligula imperdiet, tincidunt mauris ac, tincidunt eros.",
+    img: "./Rohan.jpg",
+    current: true,
+  },
 ]
 
 export default function Timeline(){
@@ -29,12 +36,20 @@ export default function Timeline(){
     <div className="w-full pr-16 md:pr-8 px-4 md:pl-break my-break">
       <motion.div 
 
-      animate={{ y: 0, opacity: 1 }} 
-      initial={{ y: offsetDistance, opacity: 0 }} 
-      transition={{ease: "backInOut", duration: animationDuration}}
+      initial={{opacity: 0, x: -100}}
+      whileInView={{opacity: 1, x: 0}}
+      transition={{duration: animationDuration, ease: "backInOut"}}
+      viewport={{once: true}}
       
       className="text-6xl text-text-light font-bold">Here's the plan.</motion.div>
-      <div className="text-2xl text-text">And the progress.</div>
+
+      <motion.div 
+      initial={{opacity: 0, x: -100}}
+      whileInView={{opacity: 1, x: 0}}
+      transition={{duration: animationDuration, ease: "backInOut"}}
+      viewport={{once: true}}
+
+      className="text-2xl text-text">And the progress.</motion.div>
       <ol className="relative border-l border-primary mt-12 flex flex-col space-y-break">
         {nodes.map((node, index) => (
           <NodeComponent node={node} stage={index + 1}/>
@@ -45,22 +60,46 @@ export default function Timeline(){
 }
 
 function NodeComponent(props: {node: TimelineNode, stage: number}){
-  const { time, title, text, img} = props.node;
+  const { time, title, text, img, current} = props.node;
   return(
     <li className="flex flex-col lg:flex-row space-y-8 lg:space-y-0 lg:space-x-8 h-fit">
       <div className="w-full">
-        <div className="relative w-full flex items-center">
+        <motion.div 
+        initial={{opacity: 0, y: offsetDistance}}
+        whileInView={{opacity: 1, y: 0}}
+        transition={{duration: animationDuration, ease: "backInOut"}}
+        viewport={{once: true}}
+
+        className="relative w-full flex items-center">
           <div className="absolute w-3 aspect-square bg-primary -translate-x-1/2 rounded-full"></div>
           <div className="text-xl text-primary font-bold ml-8">Stage {props.stage}</div>
           <div className="ml-2 text-lg">{time}</div>
-        </div>
+        </motion.div>
         <div className="ml-8">
-          <h1 className="text-2xl text-text-light font-bold">{title}</h1>
-          <div className="text-lg mt-2">{text}</div>
+          <motion.h1 
+          initial={{opacity: 0, y: offsetDistance}}
+          whileInView={{opacity: 1, y: 0}}
+          transition={{duration: animationDuration, ease: "backInOut"}}
+          viewport={{once: true}}
+
+          className="text-2xl text-text-light font-bold flex"><div>{title}</div> <div hidden={!current} className="text-sm py-1 px-2 border-2 rounded-lg ml-4 border-primary/60 text-primary">Current</div></motion.h1>
+          <motion.div 
+          initial={{opacity: 0, y: offsetDistance}}
+          whileInView={{opacity: 1, y: 0}}
+          transition={{duration: animationDuration, ease: "backInOut"}}
+          viewport={{once: true}}
+
+          className="text-lg mt-2">{text}</motion.div>
         </div>
       </div>
       <div className="ml-8 lg:ml-0 w-full h-[30vh] overflow-clip flex justify-center items-center">
-        <img src={img} className="h-full object-cover rounded-[35px] border-2 p-2 border-primary/60"/>
+        <motion.img 
+        initial={{opacity: 0, x: 100}}
+        whileInView={{opacity: 1, x: 0}}
+        transition={{duration: animationDuration, ease: "backInOut"}}
+        viewport={{once: true}}
+
+        src={img} className="h-full object-cover rounded-[35px] border-2 p-2 border-primary/60"/>
       </div>
     </li>
   )
